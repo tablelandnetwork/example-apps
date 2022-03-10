de74bff4da980e8151ba28a7061bcc23cd28836f
Joe Wagner <njwjs722@gmail.com>
2022-03-09 17:57:48 -0700

    Update sdk to new namespace

--- tableland-todo/jest.config.js
+++ tableland-todo/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   globalSetup: '<rootDir>/test/global.setup.js',
   setupFilesAfterEnv: ['./jest.setup.js'],
   moduleNameMapper: {
-    "^@textile/tableland$": "<rootDir>/test/mock_modules/tableland",
+    "^@tableland/sdk$": "<rootDir>/test/mock_modules/tableland",
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1',
     '^vue$': 'vue/dist/vue.common.js'
