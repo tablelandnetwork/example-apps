e7b8b20cc376fc15da345e22987271b3ea598f12
Joe Wagner <njwjs722@gmail.com>
2022-07-07 15:33:18 -0600

    wip: updating with deploy info and verify script

--- canvas-game/hardhat.config.ts
+++ canvas-game/hardhat.config.ts
@@ -19,9 +19,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+export const deployments: {[key: string]: string} = {
+  "polygon-mumbai": "0xabD83bC4C77d8fAE7Ca5aE423F6312a40FA09b5c"
+};
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
-
 const config: HardhatUserConfig = {
   solidity: "0.8.12",
   gasReporter: {
